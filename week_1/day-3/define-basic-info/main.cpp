3f939e0e2c5194166c9acef19a174f3f21b916f9
zolnay <zolnaybence@gmail.com>
2019-06-26 17:33:43 +0200

    "add define-basic-info"

---
+++ week_1/day-3/define-basic-info/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include  <string>
+int main() {
+
+    std::string name = "Zolnay Bence";
+    std::cout << name << std::endl;
+
+    int age = 20;
+    std::cout << age << std::endl;
+
+    double height = 1.79;
+    std::cout << height << std::endl;
+
+    bool Relations = true;
+    if (Relations != true) {
+
+        std::cout << "Married " << std::endl;
+
+    }else{
+        std::cout << "not Married" << std::endl;
+
+    }
+
+    return 0;
+}
