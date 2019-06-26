95bf3839a8551a6e976bce2086c66e602bd5776b
zolnay <zolnaybence@gmail.com>
2019-06-26 17:03:43 +0200

    "add FavoriteNumber"

---
+++ week_1/day-3/FavoriteNumber/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cin >> num;
+    std::cout << "My favorite number is: " << num << std::endl;
+    int fav = 8;
+    std::cout <<"My other favorite number is:" << fav << std::endl;
+    return 0;
+}
