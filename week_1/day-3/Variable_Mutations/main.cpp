702c7fb0653d6f14295c2565897896cd13f7df77
zolnay <zolnaybence@gmail.com>
2019-06-26 17:52:21 +0200

    "add Variable_Mutations#finish"

---
+++ week_1/day-3/Variable_Mutations/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int a = 3;
+     a = 3 + 10;
+     std::cout << a << std::endl;
+    int b = 100;
+     b = 100 - 7;
+     std::cout << b << std::endl;
+    int c = 44;
+     c = 44 * 2;
+     std::cout << c << std::endl;
+    int d = 125;
+     d = 125 / 5;
+     std::cout << d << std::endl;
+    int e = 8;
+     e = 8 * 8;
+     std::cout << e << std::endl;
+    int f1 = 123;
+    int f2 = 345;
+    bool t = true;
+    bool b = false;
+    return 0;
+}
