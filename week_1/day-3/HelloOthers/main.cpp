f76ac24c7324c8173c24b8e93d40aa44b7ff17ff
zolnay <zolnaybence@gmail.com>
2019-06-26 17:00:32 +0200

    "add HelloOthers"

---
+++ week_1/day-3/HelloOthers/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, Peti!" << std::endl;
+    std::cout << "Hello, Kornel!" << std::endl;
+    std::cout << "Hello, Tomi!" << std::endl;
+    return 0;
+}
