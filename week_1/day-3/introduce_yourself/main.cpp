b382d87dccb6e34b496953d00be9113060f0dc27
zolnay <zolnaybence@gmail.com>
2019-06-26 17:01:42 +0200

    "add Introduceyourself"

---
+++ week_1/day-3/introduce_yourself/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Zolnay Bence Attila" << std::endl;
+    int age=20;
+    std::cout << age << std::endl;
+    float height= 1.79;
+    std::cout << height << std::endl;
+
+
+    return 0;
+}
