5ceb0f8c0c81cfebe78f2dac80192faca81ef1b2
zolnay <zolnaybence@gmail.com>
2019-06-27 13:10:18 +0200

    "add HelloUser"

---
+++ week_1/day_4/HelloUser/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string User;
+    std::cout << "What's Your name?" << std::endl;
+    std::cin >> User;
+    std::cout << "Hello, "  << User <<std::endl;
+    return 0;
+}
