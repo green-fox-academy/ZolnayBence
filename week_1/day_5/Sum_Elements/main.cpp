44786231917c35975aae052bc14c7aae6eeaae34
zolnay <zolnaybence@gmail.com>
2019-06-28 14:03:08 +0200

    "add Sum_elements"

--- week_1/day_5/Sum_Elements/main.cpp
+++ week_1/day_5/Sum_Elements/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-int sum(int r[], int n){
-    int sum = 0;
-    for(int i =0; i< n; i++)
-        sum += r[i];
-    return sum;
-}
+
+
+
 int main()
 {
     int r[] = {54, 23, 66, 12};
-    int n = sizeof(r) / sizeof(r[0]);
-    std::cout << "Sum of Arrays are: " << sum(r, n);
+    std::cout <<"The sum of the 2nd and the 3rd array is: " << r[1] + r[2] << std::endl;
     return 0;
 }
