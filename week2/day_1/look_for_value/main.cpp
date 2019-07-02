0b6d673d33830482e476783faf2fd16be7638f95
zolnay <zolnaybence@gmail.com>
2019-07-02 15:56:14 +0200

    "add look_for_value"

--- week2/day_1/look_for_value/main.cpp
+++ week2/day_1/look_for_value/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-
+int funct(int arr[], int length, int k);
 int main()
 {
     // Create a function which takes an array (and it's lenght) and a number as a parameter
     // the function should return index where it found the given value
     // if it can't find the number return -1
-
+    int array[5] {23, 12, 54, 10, 6};
+    int length = (sizeof(array) / sizeof(array[0]));
+    std::cout << funct(array, length, 54) <<std::endl;
     return 0;
 }
-int *funct(int function[], int length)
-{
-    int *arrayIndex = &function[0];
-for(int i = 0; i < length; ++i)
-{
 
-}
+int funct(int arr[], int length, int k)
+{
+    for (int i = 0; i < length; ++i) {
+        if (arr[i] == k) {
+            return i;
+        }
+    }
+    return -1;
 }
