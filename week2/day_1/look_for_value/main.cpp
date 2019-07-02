76af8c9eb022d11a3b9eab97d971d05e807e066b
zolnay <zolnaybence@gmail.com>
2019-07-02 15:20:26 +0200

    "add look_for_value"

---
+++ week2/day_1/look_for_value/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // Create a function which takes an array (and it's lenght) and a number as a parameter
+    // the function should return index where it found the given value
+    // if it can't find the number return -1
+
+    return 0;
+}
+int *funct(int function[], int length)
+{
+    int *arrayIndex = &function[0];
+for(int i = 0; i < length; ++i)
+{
+
+}
+}
