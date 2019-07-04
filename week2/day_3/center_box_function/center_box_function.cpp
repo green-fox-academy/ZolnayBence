ea8c21f37960d6029fc7310f503f0fc8a87a3246
zolnay <zolnaybence@gmail.com>
2019-07-04 11:03:34 +0200

    "add center_box_function"

--- week2/day_3/center_box_function/center_box_function.cpp
+++ week2/day_3/center_box_function/center_box_function.cpp
@@ -8,20 +8,17 @@ void draw(SDL_Renderer* gRenderer) {
     // the squares should not be filled otherwise they will hide each other
     // avoid code duplication.
         for(int i = 0; i < 3; ++i) {
-        square(gRenderer,  i *36);
+        square(gRenderer,  i *30);
     }
 }
 void square(SDL_Renderer *gRenderer, int size)
 {
-    int center = 520 - size;
+    int center =520 - (size / 2);
+    int center2 = 390 - (size / 2);
 
     SDL_SetRenderDrawColor(gRenderer, 140, 200, 190, 0);
-
-    SDL_RenderDrawLine(gRenderer, size, size, size, center);
-    SDL_RenderDrawLine(gRenderer, size, center, center, center);
-    SDL_RenderDrawLine(gRenderer, center, center, center, size);
-    SDL_RenderDrawLine(gRenderer, size, size, center, size);
-
+    SDL_Rect emptyRect = { center, center2, size, size};
+    SDL_RenderDrawRect (gRenderer, &emptyRect);
 }
 
 
