1c0568e327ef16980c3a6e231e85bbeb14f3a9de
zolnay <49492028+zolnay@users.noreply.github.com>
2019-07-07 19:21:12 +0200

    "add task: divid_by_zero"

---
+++ week3/Day_1/divide_by_zero/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+int divide(int num1);
+
+int main() {
+    // Create a function that takes a number
+    // divides ten with it,
+    // and prints the result.
+    // It should print "fail" if the parameter is 0
+
+    try {
+        int num1;
+        divide(num1);
+        std::cout << "Enter a number: " << std::endl;
+        std::cin >> num1;
+        if (num1 == 0) {
+            throw 0;
+
+        } else {
+            std::cout << "the result of your division is: " << divide(num1) << std::endl;
+        }
+
+
+    } catch (int x) {
+        std::cout << "Fail ERROR: " << x << std::endl;
+        return 0;
+    }
+}
+
+int divide(int num1) {
+
+    return (num1 / 10);
+}
