2ac127014740d359f6ace7580ac49a4d87ba9652
zolnay <49492028+zolnay@users.noreply.github.com>
2019-08-01 20:30:22 +0200

    Trust me its done

--- week_5/exercise/exam-test-Animal-protection/AnimalShelter.cpp
+++ week_5/exercise/exam-test-Animal-protection/AnimalShelter.cpp
@@ -1,2 +1,55 @@
 #include "AnimalShelter.h"
 
+AnimalShelter::AnimalShelter() {}
+
+AnimalShelter::AnimalShelter(int budget) : _budget(budget) {
+    _budget = 50;
+}
+
+int AnimalShelter::rescueAnimals(Animal *animal) {
+    animalsInTheShelter.push_back(animal);
+    return animalsInTheShelter.size();
+}
+
+int AnimalShelter::heal() {
+    bool wasHealed = false;
+    int i;
+    for (int i = 0; i < animalsInTheShelter.size(); ++i) {
+        if(!animalsInTheShelter[i]->isHealthy() && _budget > animalsInTheShelter[i]->getHealCost() && !wasHealed){
+            animalsInTheShelter[i]->heal();
+            _budget -= animalsInTheShelter[i]->getHealCost();
+            wasHealed = true;
+        }
+    }
+    return wasHealed;
+}
+
+void AnimalShelter::addAdopter(std::string name) {
+    adoptersName.push_back(name);
+}
+
+void AnimalShelter::findNewOwner() {
+    if(animalsInTheShelter.size() != 0 && adoptersName.size() != 0){
+        srand(time(nullptr));
+        animalsInTheShelter.erase(animalsInTheShelter.begin() + (rand()% animalsInTheShelter.size() + 1));
+        adoptersName.erase(adoptersName.begin() + (rand()% adoptersName.size() + 1));
+    }
+}
+
+int AnimalShelter::earnDonation(int donation) {
+    _budget += donation;
+    return donation;
+}
+
+std::string AnimalShelter::toString() {
+    std::cout << "Budget: " << _budget << " Euro, "<< std::endl;
+    std::cout << "There are " << animalsInTheShelter.size() << " animal(s) and " << adoptersName.size() << " potentional adopter(s)"
+    << std::endl;
+    for (int i = 0; i < animalsInTheShelter.size(); ++i) {
+        animalsInTheShelter[i]->toString();
+    }
+}
+
+
+
+
--- week_5/exercise/exam-test-Animal-protection/Parrot.cpp
+++ week_5/exercise/exam-test-Animal-protection/Parrot.cpp
@@ -3,5 +3,15 @@
 //
 
 #include "Parrot.h"
-//srand(time(nullptr));
-//    _healCost = rand() % 7 + 4;
+
+Parrot::Parrot(const std::string &name, bool isHealthy) : Animal(name, isHealthy) {
+    srand(time(nullptr));
+    _healCost = rand() % 7;
+    std::cout << _healCost << std::endl;
+    _name = "parrot";
+}
+
+Parrot::Parrot(bool isHealthy) : Animal(isHealthy) {
+    srand(time(nullptr));
+    _healCost = rand() % 7 + 4;
+}
