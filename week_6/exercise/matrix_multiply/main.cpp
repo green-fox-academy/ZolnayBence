a65f8c9ff7a57d0e6b128ecbc56aa7915b6a3a4d
zolnay <zolnaybence@gmail.com>
2019-08-05 17:56:05 +0200

    feladat

--- week_6/exercise/matrix_multiply/main.cpp
+++ week_6/exercise/matrix_multiply/main.cpp
@@ -5,18 +5,22 @@ std::vector<std::vector<int>> matrixMultplier(std::vector<std::vector<int>> matr
 int main()
 {
     std::vector<std::vector<int>> matrix = {
-            {2, 8, 4},
-            {3, 5, 6},
-            {7, 9, 1}
+            {1, 0, 0},
+            {0, 1, 0},
+            {0, 0, 1}
     };
     std::vector<std::vector<int>> matrix2 = {
             {2, 8, 4},
             {3, 5, 6},
             {7, 9, 1}
     };
-    for (int i = 0; i < ; ++i) {
-        
+    std::vector<std::vector<int>> matrixs = matrixMultplier(matrix, matrix2);
+    for (int i = 0; i < matrixs.size(); ++i) {
+        for (int j = 0; j < matrixs[i].size(); ++j) {
+            std::cout << matrixs[i][j] << " ";
+        }
     }
+    matrixMultplier(matrix, matrix2);
     
     return 0;
 }
@@ -25,8 +29,15 @@ std::vector<std::vector<int>> matrixMultplier(std::vector<std::vector<int>> matr
     std::vector<std::vector<int>> matrixFunction = matrix;
 
     for (int i = 0; i < matrix.size(); ++i) {
+        matrixFunction.resize(matrix.size());
         for (int j = 0; j < matrix[i].size(); ++j) {
-            matrixFunction[i][j] = (matrix[j][i] * matrix2[j][i]) + matrixFunction[i][j]; 
-        }
+            matrixFunction[0].resize(matrix[0].size());
+
+            matrixFunction[i][j] += (matrix[j][i] + 1) * matrix2[i][j];
+
+
+            }
+
     }
+    return matrixFunction;
 }
